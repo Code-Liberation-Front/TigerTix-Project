35f3f9e5230cfac29537f71b67af76dc69d417a3
moyito2604 <moyito2604@gmail.com>
2024-10-10 12:39:51 -0400

    Pushing MVC framework

---
+++ Controllers/AppController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TigerTix.Web.Models;
+
+namespace TigerTix.Web.Controllers
+{
+
+    public class AppController : Controller
+    {
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost("/App")]
+        public IActionResult Index(IndexViewModel model)
+        {
+			return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -9,5 +13,15 @@ if (!app.Environment.IsDevelopment())
 
 //app.MapGet("/", () => "Hello World!");
 app.UseDefaultFiles();
+app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=App}/{action=Index}/{id?}");
+
 app.Run();
