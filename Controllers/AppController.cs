fc1ce02c72b2fcfd066b44c38b5cf370e1c60e55
Mobro <39844165+moyito2604@users.noreply.github.com>
2024-10-11 16:00:52 -0400

    Login Redirects to HomePage
    Homepage is now the ticket page

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -15,7 +15,7 @@ namespace TigerTix.Web.Controllers
         public IActionResult LoginView(LoginViewModel model)
         {
             if (model.Authentication()) {
-                return RedirectToAction("TicketView");
+                return RedirectToAction("Index");
             }
             return RedirectToAction("LoginView");
         }
